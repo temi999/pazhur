9b7808d58e3e222c409146dfae0bc795c250336c
Vitaliy <temi999@github.com>
2019-11-23 20:04:03 +0300

    Added files and changes required for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pazhur.wsgi --log-file -
--- pazhur/settings.py
+++ pazhur/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2v0k!uf=g&x9t&y29na6=_p2kl@17#+7gvhx3g48061iz_gdv3'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '2v0k!uf=g&x9t&y29na6=_p2kl@17#+7gvhx3g48061iz_gdv3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = []
 
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
