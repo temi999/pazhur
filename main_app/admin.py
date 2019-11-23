2e002d271929c2f89cba0e46dcfaa955aeef1f22
Vitaliy <temi999@github.com>
2019-11-24 02:13:13 +0300

    [fix] Removed all models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import (
-    Reel,
-    ReelSet,
-    DefaultReel,
-    DefaultReelSet
-)
+# from .models import (
+#     Reel,
+#     ReelSet,
+#     DefaultReel,
+#     DefaultReelSet
+# )
 
 # Register your models here.
-admin.site.register(Reel)
-admin.site.register(ReelSet)
-admin.site.register(DefaultReel)
-admin.site.register(DefaultReelSet)
+# admin.site.register(Reel)
+# admin.site.register(ReelSet)
+# admin.site.register(DefaultReel)
+# admin.site.register(DefaultReelSet)
