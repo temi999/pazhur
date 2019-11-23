8cdc34b3dcbdd3fa8d878c3415841a899dd6d743
Vitaliy <temi999@github.com>
2019-11-23 20:35:52 +0300

    [fix] Added heroku as allowed host

--- pazhur/settings.py
+++ pazhur/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '2v0k!uf=g&x9t&y29na6=_p2kl@17#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pazhur.herokuapp.com']
 
 
 # Application definition
