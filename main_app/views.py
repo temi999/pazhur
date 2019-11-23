1dcfdacc8e2450128266d226b2174f4a5b62ad55
Vitaliy <temi999@github.com>
2019-11-24 02:24:18 +0300

    [fix] Fixed database, now it works

--- main_app/views.py
+++ main_app/views.py
@@ -5,16 +5,16 @@ from .models import *
 # Create your views here.
 def index(request):
     context = {
-        # 'all_reelsets': ReelSet.objects.all(),
-        # 'all_default_reelsets': DefaultReelSet.objects.all(),
+        'all_reelsets': ReelSet.objects.all(),
+        'all_default_reelsets': DefaultReelSet.objects.all(),
     }
     return render(request, 'index.html', context)
 
 
 def reel_sets(request):
     context = {
-        # 'all_reelsets': ReelSet.objects.all(),
-        # 'all_default_reelsets': DefaultReelSet.objects.all(),
+        'all_reelsets': ReelSet.objects.all(),
+        'all_default_reelsets': DefaultReelSet.objects.all(),
     }
 
     return render(request, 'reel_sets.html', context)
